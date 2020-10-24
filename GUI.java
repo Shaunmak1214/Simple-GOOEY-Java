109c835a31aa45e925994b6388c558817d2a8d2a
Shaunmak1214 <shaonmak@gmail.com>
2020-10-25 01:26:54 +0800

    Done

--- GUI.java
+++ GUI.java
@@ -1,23 +1,44 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.*;
 
-import javax.swing.BorderFactory;
+public class GUI implements ActionListener{
 
-public class GUI {
+    private int count  = 0;
+    JLabel label;
+    JFrame frame;
+    JPanel panel;
+    JButton button;
 
     //default constructor
     public GUI() {
 
-        //Frame configurations
-        JFrame frame = new JFrame();
+        //Frame Object
+        frame = new JFrame();
 
-        //Panel Configurations
-        JPanel panel = new JPanel();
+        //Button Object
+        button = new JButton("Click Me");
+        button.addActionListener(this);
+
+        //Label Object
+        label = new JLabel("Number of clicks: 0");
+
+        //Panel Object
+        panel = new JPanel();
+
+        //Panel Setup
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
 
+        //Frame Setup
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Our GUI");
@@ -29,6 +50,14 @@ public class GUI {
     public static void main(String [] args) {
 
         new GUI();
+
+    }
+
+    //Overiding
+    public void actionPerformed(ActionEvent e) {
+
+        count++;
+        label.setText("Number of clicks: " + count);
 
     }
 
